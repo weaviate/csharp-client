6ff246e78e1b1d41358e0ddde16d015db0a43de0
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-04-10 13:58:50 +0200

    Delete unnecessary files

--- src/Weaviate.Client/Rest/Response/CreateCollectionResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace Weaviate.Client.Rest.Responses;
-
-using Weaviate.Client.Models;
-using Weaviate.Client.Rest.Models;
-
-public struct CreateCollectionResponse
-{
-    public Collection? Collection { get; internal set; }
-}
