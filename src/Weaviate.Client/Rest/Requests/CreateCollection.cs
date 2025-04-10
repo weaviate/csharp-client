0530db4c31b8960c0d85665b9989404992b191f6
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-04-10 13:58:50 +0200

    Delete unnecessary files

--- src/Weaviate.Client/Rest/Requests/CreateCollection.cs
+++
@@ -1,9 +0,0 @@
-namespace Weaviate.Client.Rest.Requests;
-
-using Weaviate.Client.Models;
-using Weaviate.Client.Rest.Models;
-
-public class CreateCollectionRequest
-{
-    public required Collection Collection { get; set; }
-}
