9c7b326a3dd701a031ed07e1929868d8d3b9a3e0
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-04-23 16:34:42 +0200

    - Add type information to collection #1 - Add .Data and .Query scopes to CollectionClient (#2) - Add basic NearVector request

--- src/Weaviate.Client/CollectionsClient.cs
+++ src/Weaviate.Client/CollectionsClient.cs
@@ -1,72 +1,5 @@
 namespace Weaviate.Client;
 
-internal static class WeaviateClientExtensions
-{
-    internal static Models.Collection ToCollection(this Rest.Dto.CollectionGeneric collection)
-    {
-        return new Models.Collection()
-        {
-            Name = collection.Class,
-            Description = collection.Description,
-            Properties = collection.Properties.Select(p => new Models.Property()
-            {
-                Name = p.Name,
-                DataType = p.DataType.ToList()
-            }).ToList(),
-            InvertedIndexConfig = (collection.InvertedIndexConfig is Rest.Dto.InvertedIndexConfig iic)
-                ? new Models.InvertedIndexConfig()
-                {
-                    Bm25 = iic.Bm25 == null ? null : new Models.BM25Config
-                    {
-                        B = iic.Bm25.B,
-                        K1 = iic.Bm25.K1,
-                    },
-                    Stopwords = (iic.Stopwords is Rest.Dto.StopwordConfig swc)
-                    ? new Models.StopwordConfig
-                    {
-                        Additions = swc.Additions,
-                        Preset = swc.Preset,
-                        Removals = swc.Removals,
-                    } : null,
-                    CleanupIntervalSeconds = iic.CleanupIntervalSeconds,
-                    IndexNullState = iic.IndexNullState,
-                    IndexPropertyLength = iic.IndexPropertyLength,
-                    IndexTimestamps = iic.IndexTimestamps,
-                } : null,
-            ShardingConfig = collection.ShardingConfig,
-            ModuleConfig = collection.ModuleConfig,
-            ReplicationConfig = (collection.ReplicationConfig is Rest.Dto.ReplicationConfig rc)
-                ? new Models.ReplicationConfig
-                {
-                    AsyncEnabled = rc.AsyncEnabled,
-                    Factor = rc.Factor,
-                    DeletionStrategy = (Models.DeletionStrategy?)rc.DeletionStrategy,
-                } : null,
-            MultiTenancyConfig = (collection.MultiTenancyConfig is Rest.Dto.MultiTenancyConfig mtc)
-                ? new Models.MultiTenancyConfig
-                {
-                    Enabled = mtc.Enabled,
-                    AutoTenantActivation = mtc.AutoTenantActivation,
-                    AutoTenantCreation = mtc.AutoTenantCreation,
-                } : null,
-            VectorConfig =
-                collection.VectorConfig?.ToList()
-                .ToDictionary(
-                    e => e.Key,
-                    e => new Models.VectorConfig
-                    {
-                        VectorIndexConfig = e.Value.VectorIndexConfig,
-                        VectorIndexType = e.Value.VectorIndexType,
-                        Vectorizer = e.Value.Vectorizer,
-                    }
-                    ) ?? new Dictionary<string, Models.VectorConfig>(),
-            Vectorizer = collection.Vectorizer,
-            VectorIndexType = collection.VectorIndexType,
-            VectorIndexConfig = collection.VectorIndexConfig,
-        };
-    }
-}
-
 public struct CollectionsClient
 {
     private readonly WeaviateClient _client;
@@ -76,109 +9,15 @@ public struct CollectionsClient
         _client = client;
     }
 
-    public async Task<CollectionClient> Create(Action<Models.Collection> collectionConfigurator)
+    public async Task<CollectionClient<TData>> Create<TData>(Action<Models.Collection> collectionConfigurator)
     {
         var collection = new Models.Collection();
 
         collectionConfigurator(collection);
 
-        var data = new Rest.Dto.CollectionGeneric()
-        {
-            Class = collection.Name,
-            Description = collection.Description,
-            Properties = new List<Rest.Dto.Property>(),
-            VectorConfig = collection.VectorConfig?.ToList()
-                .ToDictionary(
-                    e => e.Key,
-                    e => new Rest.Dto.VectorConfig
-                    {
-                        VectorIndexConfig = e.Value.VectorIndexConfig,
-                        VectorIndexType = e.Value.VectorIndexType,
-                        Vectorizer = e.Value.Vectorizer,
-                    }
-                    ) ?? new Dictionary<string, Rest.Dto.VectorConfig>(),
-            ShardingConfig = collection.ShardingConfig,
-            ModuleConfig = collection.ModuleConfig,
-            VectorIndexType = collection.VectorIndexType,
-            VectorIndexConfig = collection.VectorIndexConfig,
-            Vectorizer = collection.Vectorizer,
-        };
+        var response = await _client.RestClient.CollectionCreate(collection.ToDto());
 
-        foreach (var property in collection.Properties)
-        {
-            data.Properties.Add(new Rest.Dto.Property()
-            {
-                Name = property.Name,
-                DataType = [.. property.DataType]
-            });
-        }
-
-        if (collection.ReplicationConfig is Models.ReplicationConfig rc)
-        {
-            data.ReplicationConfig = new Rest.Dto.ReplicationConfig()
-            {
-                AsyncEnabled = rc.AsyncEnabled,
-                DeletionStrategy = (Rest.Dto.DeletionStrategy?)rc.DeletionStrategy,
-                Factor = rc.Factor
-            };
-        }
-
-        if (collection.MultiTenancyConfig is Models.MultiTenancyConfig mtc)
-        {
-            data.MultiTenancyConfig = new Rest.Dto.MultiTenancyConfig()
-            {
-                AutoTenantActivation = mtc.AutoTenantActivation,
-                AutoTenantCreation = mtc.AutoTenantCreation,
-                Enabled = mtc.Enabled,
-            };
-        }
-
-        if (collection.InvertedIndexConfig != null)
-        {
-            data.InvertedIndexConfig = new Rest.Dto.InvertedIndexConfig()
-            {
-                Bm25 = collection.InvertedIndexConfig.Bm25 == null ? null : new Rest.Dto.BM25Config
-                {
-                    B = collection.InvertedIndexConfig.Bm25.B,
-                    K1 = collection.InvertedIndexConfig.Bm25.K1,
-                },
-                Stopwords = collection.InvertedIndexConfig.Stopwords == null ? null : new Rest.Dto.StopwordConfig
-                {
-                    Additions = collection.InvertedIndexConfig.Stopwords.Additions,
-                    Preset = collection.InvertedIndexConfig.Stopwords.Preset,
-                    Removals = collection.InvertedIndexConfig.Stopwords.Removals,
-                },
-                CleanupIntervalSeconds = collection.InvertedIndexConfig.CleanupIntervalSeconds,
-                IndexNullState = collection.InvertedIndexConfig.IndexNullState,
-                IndexPropertyLength = collection.InvertedIndexConfig.IndexPropertyLength,
-                IndexTimestamps = collection.InvertedIndexConfig.IndexTimestamps,
-            };
-        }
-
-        var response = await _client.RestClient.CollectionCreate(data);
-
-        return new CollectionClient(_client, response.ToCollection());
-    }
-
-    public async Task Delete(string name)
-    {
-        await _client.RestClient.CollectionDelete(name);
-    }
-
-    public CollectionClient this[string name] => Get(name).Result;
-
-    public async Task<CollectionClient> Get(string name)
-    {
-        var response = await _client.RestClient.CollectionGet(name);
-
-        if (response is null)
-        {
-            return null!;
-        }
-
-        var collection = new CollectionClient(_client, response.ToCollection());
-
-        return collection;
+        return new CollectionClient<TData>(_client, response.ToModel());
     }
 
     public async IAsyncEnumerable<Models.Collection> List()
@@ -187,7 +26,14 @@ public struct CollectionsClient
 
         foreach (var c in response.Collections)
         {
-            yield return c.ToCollection();
+            yield return c.ToModel();
         }
+    }
+
+    public CollectionClient<TData> Use<TData>(string? name = null)
+    {
+        name = name ?? typeof(TData).Name;
+
+        return new CollectionClient<TData>(_client, name);
     }
 }
