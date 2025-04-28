d909d61a6115f63add8b0f8afcb71d2bc1e51c7e
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-04-28 15:03:40 +0200

    Very rough working NearVector search

--- src/Weaviate.Client/QueryClient.cs
+++ src/Weaviate.Client/QueryClient.cs
@@ -1,8 +1,12 @@
+using Weaviate.Client.Grpc;
+using Weaviate.Client.Models;
+
 namespace Weaviate.Client;
 
 public class QueryClient<TData>
 {
     private readonly CollectionClient<TData> _collectionClient;
+    private string _collectionName => _collectionClient.Name;
 
     private WeaviateClient _client => _collectionClient.Client;
 
@@ -11,16 +15,49 @@ public class QueryClient<TData>
         _collectionClient = collectionClient;
     }
 
+
+    #region Objects
+    public async IAsyncEnumerable<WeaviateObject<TData>> List(uint? limit = null)
+    {
+        var list = await _client.GrpcClient.FetchObjects(_collectionName, limit: limit);
+
+        foreach (var data in list.ToObjects<TData>())
+        {
+            yield return data;
+        }
+    }
+
+    public async Task<WeaviateObject<TData>?> FetchObjectByID(Guid id)
+    {
+        var reply = await _client.GrpcClient.FetchObjects(_collectionName, Filter.WithID(id));
+
+        var data = reply.FirstOrDefault();
+
+        if (data is null)
+        {
+            return null;
+        }
+
+        return data.ToWeaviateObject<TData>();
+    }
+
+    public async IAsyncEnumerable<WeaviateObject<TData>> FetchObjectsByIDs(ISet<Guid> ids, uint? limit = null)
+    {
+        var list = await _client.GrpcClient.FetchObjects(_collectionName, limit: limit, filter: Filter.WithIDs(ids));
+
+        foreach (var data in list.ToObjects<TData>())
+        {
+            yield return data;
+        }
+    }
+    #endregion
+
+    #region Search
+
     public async Task<object> NearText(string text, int? limit = null)
     {
         return await Task.FromResult(new object { });
     }
-
-    // public async IAsyncEnumerable<WeaviateObject<TData>> NearVector(float[] vector, uint? limit, string[]? fields, string[]? metadata)
-    // {
-    //     await foreach (var r in NearVector<TData>(vector, limit: limit, fields: fields, metadata: metadata))
-    //         yield return r;
-    // }
 
     public async IAsyncEnumerable<Rest.Dto.WeaviateObject> NearVector(float[] vector, float? distance = null, float? certainty = null, uint? limit = null, string[]? fields = null, string[]? metadata = null)
     {
@@ -37,4 +74,6 @@ public class QueryClient<TData>
             yield return r;
         }
     }
+
+    #endregion
 }
