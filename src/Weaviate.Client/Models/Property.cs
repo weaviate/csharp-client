23d7cd791db927f63c13aba6efd9f41fc8a1e850
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-05-19 16:55:49 +0200

    Increased Filter support

--- src/Weaviate.Client/Models/Property.cs
+++ src/Weaviate.Client/Models/Property.cs
@@ -1,13 +1,17 @@
-
-
-
 namespace Weaviate.Client.Models;
 
 public static class DataType
 {
-    public static string Text { get; } = "text";
-    public static string Int { get; } = "int";
     public static string Date { get; } = "date";
+    public static string GeoCoordinate { get; } = "geo";
+    public static string Int { get; } = "int";
+    public static string List { get; } = "list";
+    public static string Number { get; } = "number";
+    public static string Object { get; } = "object";
+    public static string PhoneNumber { get; } = "phone";
+    public static string Text { get; } = "text";
+    public static string Uuid { get; } = "uuid";
+
     public static string Reference(string property) => property.Capitalize();
 }
 
@@ -21,7 +25,7 @@ public class ReferenceProperty
         return new Property
         {
             Name = p.Name,
-            DataType = { DataType.Reference(p.TargetCollection) }
+            DataType = { DataType.Reference(p.TargetCollection) },
         };
     }
 }
@@ -38,38 +42,62 @@ public class Property
 
     public static Property Text(string name)
     {
-        return new Property
-        {
-            Name = name,
-            DataType = { Models.DataType.Text },
-        };
+        return new Property { Name = name, DataType = { Models.DataType.Text } };
     }
 
     public static Property Int(string name)
     {
-        return new Property
-        {
-            Name = name,
-            DataType = { Models.DataType.Int },
-        };
+        return new Property { Name = name, DataType = { Models.DataType.Int } };
     }
-
 
     public static Property Date(string name)
     {
-        return new Property
-        {
-            Name = name,
-            DataType = { Models.DataType.Date },
-        };
+        return new Property { Name = name, DataType = { Models.DataType.Date } };
+    }
+
+    public static Property Number(string name)
+    {
+        return new Property { Name = name, DataType = { Models.DataType.Number } };
     }
 
     public static ReferenceProperty Reference(string name, string targetCollection)
     {
-        return new ReferenceProperty
-        {
-            Name = name,
-            TargetCollection = targetCollection
-        };
+        return new ReferenceProperty { Name = name, TargetCollection = targetCollection };
+    }
+
+    // Extract collection properties from type specified by TData.
+    public static IList<Property> FromType<TData>()
+    {
+        return
+        [
+            .. typeof(TData)
+                .GetProperties()
+                .Select(x =>
+                    Type.GetTypeCode(x.PropertyType) switch
+                    {
+                        TypeCode.String => Property.Text(x.Name),
+                        TypeCode.Int16 => Property.Int(x.Name),
+                        TypeCode.UInt16 => Property.Int(x.Name),
+                        TypeCode.Int32 => Property.Int(x.Name),
+                        TypeCode.UInt32 => Property.Int(x.Name),
+                        TypeCode.Int64 => Property.Int(x.Name),
+                        TypeCode.UInt64 => Property.Int(x.Name),
+                        TypeCode.DateTime => Property.Date(x.Name),
+                        TypeCode.Boolean => null,
+                        TypeCode.Char => null,
+                        TypeCode.SByte => null,
+                        TypeCode.Byte => null,
+                        TypeCode.Single => null,
+                        TypeCode.Double => null,
+                        TypeCode.Decimal => null,
+                        TypeCode.Empty => null,
+                        TypeCode.Object => null,
+                        TypeCode.DBNull => null,
+                        _ => null,
+                    }
+                )
+                .Where(p => p is not null)
+                .Select(p => p!),
+        ];
     }
 }
