c50e791bfc3921f15e77886fb2c6ab8ad21cf731
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-05-25 05:46:45 +0200

    Reference counts

--- src/Weaviate.Client/Models/ObjectReference.cs
+++ src/Weaviate.Client/Models/ObjectReference.cs
@@ -1,13 +1,18 @@
 namespace Weaviate.Client.Models;
 
-public record ObjectReference(string Name, Guid TargetID)
+public record ObjectReference(string Name, params Guid[] TargetID)
 {
+    public static implicit operator ObjectReference((string Name, Guid[] TargetID) value)
+    {
+        return new ObjectReference(value.Name, value.TargetID);
+    }
+
     public static implicit operator ObjectReference((string Name, Guid TargetID) value)
     {
         return new ObjectReference(value.Name, value.TargetID);
     }
 
-    public static implicit operator (string Name, Guid Id)(ObjectReference value) =>
+    public static implicit operator (string Name, Guid[] ID)(ObjectReference value) =>
         (value.Name, value.TargetID);
 
     public static implicit operator List<ObjectReference>(ObjectReference value) =>
