326372f2306c35597350e27da2bd80c56ea3c3a4
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-06-24 19:15:52 +0200

    CI scripts

--- ci/stop_weaviate.sh
+++ ci/stop_weaviate.sh
@@ -4,7 +4,10 @@ set -eou pipefail
 
 export WEAVIATE_VERSION=$1
 
-source ./ci/compose.sh
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# shellcheck disable=SC1091
+source compose.sh
 
 compose_down_all
 rm -rf weaviate-data || true
