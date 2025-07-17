46ba307d30f4fb6af981427fc2df8e0b270fd5e1
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-07-17 21:36:56 +0200

    Refactor filter length method and add new filter tests + nearvector

--- src/Weaviate.Client.Tests/Integration/TestCollectionAggregate.cs
+++ src/Weaviate.Client.Tests/Integration/TestCollectionAggregate.cs
@@ -29,38 +29,6 @@ public partial class AggregatesTests : IntegrationTests
         Assert.Equal(howMany, result.TotalCount);
     }
 
-    // [Theory]
-    // [InlineData(1)]
-    // [InlineData(10000)]
-    // [InlineData(20000)]
-    // [InlineData(20001)]
-    // [InlineData(100000)]
-    // public async Task Test_Collection_Length_Tenant(int howMany)
-    // {
-    //     var collectionClient = await CollectionFactory(
-    //     vectorConfig: Configure.Vectors.SelfProvided(),
-    //     multiTenancyConfig: new MultiTenancyConfig {
-    //         Enabled = true,
-    //     }
-    //     );
-
-    //     await collectionClient.Tenants.Create(new[]
-    //     {
-    //         new Tenant("tenant1"),
-    //         new Tenant("tenant2"),
-    //         new Tenant("tenant3")
-    //     });
-
-    //     var tenant2Client = collectionClient.WithTenant("tenant2");
-    //     await tenant2Client.Data.InsertMany(Enumerable.Range(0, howMany * 2).Select(_ => new { }).ToList());
-
-    //     var resultTenant2 = await tenant2Client.Aggregate.OverAll();
-    //     Assert.Equal(howMany * 2, resultTenant2.TotalCount);
-
-    //     var resultTenant3 = await collectionClient.WithTenant("tenant3").Aggregate.OverAll();
-    //     Assert.Equal(0, resultTenant3.TotalCount);
-    // }
-
     [Fact]
     public async Task Test_Empty_Aggregation()
     {
@@ -139,5 +107,218 @@ public partial class AggregatesTests : IntegrationTests
         );
 
         Assert.Empty(result.Groups);
+    }
+
+    public static IEnumerable<object[]> FilterTestData()
+    {
+        var uuid1 = _reusableUuids[0];
+        var uuid2 = _reusableUuids[1];
+        var date1 = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var date2 = new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc);
+
+        yield return new object[] { Filter.Property("text").Equal("two") };
+        yield return new object[] { Filter.Property("int").Equal(2) };
+        yield return new object[] { Filter.Property("float").Equal(2.0) };
+        yield return new object[] { Filter.Property("bool").Equal(false) };
+        yield return new object[] { Filter.Property("date").Equal(date2) };
+        yield return new object[]
+        {
+            Filter.Property("text").Equal("two") | Filter.Property("int").Equal(2),
+        };
+        yield return new object[] { Filter.Property("uuid").Equal(uuid2) };
+        yield return new object[] { Filter.Property("texts").ContainsAny(new[] { "two" }) };
+        yield return new object[] { Filter.Property("ints").ContainsAny(new[] { 2 }) };
+        yield return new object[] { Filter.Property("floats").ContainsAny(new[] { 2.0 }) };
+        yield return new object[] { Filter.Property("bools").ContainsAny(new[] { false }) };
+        yield return new object[] { Filter.Property("dates").ContainsAny(new[] { date2 }) };
+        yield return new object[] { Filter.Property("uuids").ContainsAny(new[] { uuid2 }) };
+    }
+
+    [Theory]
+    [MemberData(nameof(FilterTestData))]
+    public async Task Test_OverAll_With_Filters(Filter filter)
+    {
+        var uuid1 = _reusableUuids[0];
+        var uuid2 = _reusableUuids[1];
+        var date1 = new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var date2 = new DateTime(2021, 1, 2, 0, 0, 0, DateTimeKind.Utc);
+
+        var collectionClient = await CollectionFactory(
+            properties: new[]
+            {
+                Property.Text("text"),
+                Property.Int("int"),
+                Property.Number("float"),
+                Property.Bool("bool"),
+                Property.Date("date"),
+                Property.Uuid("uuid"),
+                Property.TextArray("texts"),
+                Property.IntArray("ints"),
+                Property.NumberArray("floats"),
+                Property.BoolArray("bools"),
+                Property.DateArray("dates"),
+                Property.UuidArray("uuids"),
+            }
+        );
+
+        await collectionClient.Data.Insert(
+            new
+            {
+                text = "one",
+                @int = 1,
+                @float = 1.0,
+                @bool = true,
+                date = date1,
+                uuid = uuid1,
+                texts = new[] { "one" },
+                ints = new[] { 1 },
+                floats = new[] { 1.0 },
+                bools = new[] { true },
+                dates = new[] { date1 },
+                uuids = new[] { uuid1 },
+            }
+        );
+
+        await collectionClient.Data.Insert(
+            new
+            {
+                text = "two",
+                @int = 2,
+                @float = 2.0,
+                @bool = false,
+                date = date2,
+                uuid = uuid2,
+                texts = new[] { "two" },
+                ints = new[] { 2 },
+                floats = new[] { 2.0 },
+                bools = new[] { false },
+                dates = new[] { date2 },
+                uuids = new[] { uuid2 },
+            }
+        );
+
+        var result = await collectionClient.Aggregate.OverAll(
+            filter: filter,
+            metrics: new[]
+            {
+                Metrics.ForProperty("text").Text(count: true, topOccurrencesValue: true),
+            }
+        );
+
+        var text = result.Properties["text"] as Aggregate.Text;
+        Assert.NotNull(text);
+        Assert.Equal(1, text.Count);
+        Assert.Equal("two", text.TopOccurrences[0].Value);
+    }
+
+    public static IEnumerable<object[]> NearVectorAggregationTestData()
+    {
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "objectLimit", 1 } },
+            1,
+        };
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "certainty", 0.9 } },
+            1,
+        };
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "distance", 0.1 } },
+            1,
+        };
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "objectLimit", 2 } },
+            2,
+        };
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "certainty", 0.1 } },
+            2,
+        };
+        yield return new object[]
+        {
+            new Dictionary<string, object> { { "distance", 0.9 } },
+            2,
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(NearVectorAggregationTestData))]
+    public async Task Test_Near_Vector_Aggregation(
+        Dictionary<string, object> option,
+        int expectedLen
+    )
+    {
+        var collectionClient = await CollectionFactory(
+            properties: [Property.Text("text")],
+            vectorConfig: Configure
+                .Vectors.Text2VecContextionary(vectorizeCollectionName: false)
+                .New("default")
+        );
+
+        var text1 = "some text";
+        var text2 = "nothing like the other one at all, not even a little bit";
+        var uuid = await collectionClient.Data.Insert(new { text = text1 });
+        var obj = await collectionClient.Query.FetchObjectByID(
+            uuid,
+            metadata: MetadataOptions.Vector
+        );
+        Assert.NotNull(obj);
+        Assert.True(obj.Vectors.ContainsKey("default"));
+        await collectionClient.Data.Insert(new { text = text2 });
+
+        var nearVector = obj.Vectors["default"].ToArray();
+        var metrics = new[]
+        {
+            Metrics
+                .ForProperty("text")
+                .Text(count: true, topOccurrencesCount: true, topOccurrencesValue: true),
+        };
+
+        AggregateResult? result = null;
+
+        if (option.ContainsKey("objectLimit"))
+        {
+            result = await collectionClient.Aggregate.NearVector(
+                nearVector,
+                metrics: metrics,
+                limit: Convert.ToUInt32(option["objectLimit"])
+            );
+        }
+        else if (option.ContainsKey("certainty"))
+        {
+            result = await collectionClient.Aggregate.NearVector(
+                nearVector,
+                metrics: metrics,
+                certainty: Convert.ToDouble(option["certainty"])
+            );
+        }
+        else if (option.ContainsKey("distance"))
+        {
+            result = await collectionClient.Aggregate.NearVector(
+                nearVector,
+                metrics: metrics,
+                distance: Convert.ToDouble(option["distance"])
+            );
+        }
+
+        Assert.NotNull(result);
+        var textAgg = result.Properties["text"] as Aggregate.Text;
+
+        Assert.NotNull(textAgg);
+        Assert.Equal(expectedLen, textAgg.Count);
+        Assert.Equal(expectedLen, textAgg.TopOccurrences.Count);
+        Assert.Contains(text1, textAgg.TopOccurrences.Select(o => o.Value));
+        if (expectedLen == 2)
+        {
+            Assert.Contains(text2, textAgg.TopOccurrences.Select(o => o.Value));
+        }
+        else
+        {
+            Assert.DoesNotContain(text2, textAgg.TopOccurrences.Select(o => o.Value));
+        }
     }
 }
