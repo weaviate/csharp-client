355c7b9872cea8e0cb6c8ff94d6f2fc650b72dfb
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-07-22 15:26:57 +0200

    Relocate multi-vector vectorizers

---
+++ src/Weaviate.Client/Configure/Vectorizer.Multivector.cs
@@ -0,0 +1,49 @@
+using Weaviate.Client.Models;
+using static Weaviate.Client.Configure.Vectors;
+
+namespace Weaviate.Client;
+
+public static partial class Configure
+{
+    public static class MultiVectors
+    {
+        public static VectorConfig SelfProvided(
+            string name = "default",
+            VectorIndexConfig? indexConfig = null
+        ) => new(name, new Vectorizer.SelfProvided(), indexConfig);
+    }
+
+    public static VectorConfigBuilder Multi2VecJinaAI(
+        string? baseURL = null,
+        int? dimensions = null,
+        string[]? imageFields = null,
+        string? model = null,
+        string[]? textFields = null,
+        bool? vectorizeCollectionName = null,
+        Vectorizer.Multi2VecJinaAIWeights? weights = null
+    ) =>
+        new(
+            new Vectorizer.Multi2VecJinaAI
+            {
+                BaseURL = baseURL,
+                Dimensions = dimensions,
+                ImageFields = imageFields,
+                Model = model,
+                TextFields = textFields,
+                VectorizeCollectionName = vectorizeCollectionName,
+                Weights = weights,
+            }
+        );
+
+    public static VectorConfigBuilder Text2VecJinaAI(
+        string? model = null,
+        bool? vectorizeCollectionName = null
+    ) =>
+        new(
+            new Vectorizer.Text2VecJinaAI
+            {
+                Model = model,
+                VectorizeCollectionName = vectorizeCollectionName,
+            }
+        );
+}
