de68bdb7cddad9e02d3ca41f1d0fc1fa88edcc2d
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-08-01 13:12:35 +0200

    Move code

--- src/Weaviate.Client/QueryClient.cs
+++ src/Weaviate.Client/QueryClient.cs
@@ -2,34 +2,6 @@ using Weaviate.Client.Models;
 
 namespace Weaviate.Client;
 
-public interface IHybridVectorInput
-{
-    // This interface is used to mark hybrid vectors, which can be either near vector or near text.
-    // It allows for polymorphic behavior in the Hybrid methods.
-}
-
-public record HybridNearVector(
-    Vectors? Vector,
-    float? Distance = null,
-    float? Certainty = null,
-    string[]? TargetVector = null
-) : IHybridVectorInput { };
-
-public record HybridNearText(
-    string Query,
-    float? Distance = null,
-    float? Certainty = null,
-    Move? MoveTo = null,
-    Move? MoveAway = null
-) : IHybridVectorInput;
-
-public abstract record BM25Operator(string Operator)
-{
-    public record And() : BM25Operator("And");
-
-    public record Or(int MinimumMatch) : BM25Operator("Or");
-}
-
 public class QueryClient<TData>
 {
     private readonly CollectionClient<TData> _collectionClient;
