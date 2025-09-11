ebb0acd5b4d852379ea279f7eb9104fc506d720d
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-09-11 16:06:21 +0200

    Add helper to accept single item or array

---
+++ src/Weaviate.Client/Models/OneOrManyOf.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+
+namespace Weaviate.Client.Models;
+
+public class OneOrManyOf<T> : IEnumerable<T>
+{
+    private readonly List<T> _items;
+
+    public OneOrManyOf(IEnumerable<T> items)
+    {
+        _items = new(items);
+    }
+
+    public OneOrManyOf(params T[] item)
+    {
+        _items = new(item);
+    }
+
+    public static implicit operator OneOrManyOf<T>(T item) => new OneOrManyOf<T>(item);
+
+    public static implicit operator OneOrManyOf<T>(T[] items) => new OneOrManyOf<T>(items);
+
+    public static implicit operator OneOrManyOf<T>(List<T> items) => new OneOrManyOf<T>(items);
+
+    public static explicit operator T[](OneOrManyOf<T> list) => list._items.ToArray();
+
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
+
+    public void Add(T item)
+    {
+        _items.Add(item);
+    }
+
+    public void Add(params T[] items)
+    {
+        if (items == null)
+            return;
+        _items.AddRange(items);
+    }
+}
