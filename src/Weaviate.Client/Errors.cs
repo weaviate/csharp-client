a6c3e35bc55f0b52fd135c2c02813e3225dd9cb6
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-09-18 13:23:00 +0200

    Enhance VectorConfig to support quantizer configuration and enrich vector index settings

--- src/Weaviate.Client/Errors.cs
+++ src/Weaviate.Client/Errors.cs
@@ -1,10 +1,31 @@
 namespace Weaviate.Client;
 
+[Serializable]
 public class WeaviateException : Exception
 {
-    public WeaviateException(Exception innerException)
+    protected WeaviateException(Exception innerException)
         : base(innerException.Message, innerException) { }
 
-    public WeaviateException(string? message, Exception? innerException = null)
+    protected WeaviateException(string? message, Exception? innerException = null)
+        : base(message, innerException) { }
+}
+
+[Serializable]
+public class WeaviateClientException : WeaviateException
+{
+    public WeaviateClientException(string? message)
+        : base(message) { }
+
+    public WeaviateClientException(string? message, Exception? innerException)
+        : base(message, innerException) { }
+}
+
+[Serializable]
+public class WeaviateServerException : WeaviateException
+{
+    public WeaviateServerException(string? message)
+        : base(message) { }
+
+    public WeaviateServerException(string? message, Exception? innerException)
         : base(message, innerException) { }
 }
