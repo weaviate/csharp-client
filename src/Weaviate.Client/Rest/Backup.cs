5b5274a26c3124a34b7da5fe527383c5e03875b8
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-10-17 12:33:58 +0200

    Refactor backup handling to use BackupStorage enum for backend type and update related methods

--- src/Weaviate.Client/Rest/Backup.cs
+++ src/Weaviate.Client/Rest/Backup.cs
@@ -1,17 +1,17 @@
 using System.Net.Http.Json;
-using Weaviate.Client.Rest.Dto;
+using Weaviate.Client.Models;
 
 namespace Weaviate.Client.Rest;
 
 public partial class WeaviateRestClient
 {
     internal async Task<Dto.BackupCreateResponse> BackupCreate(
-        string backend,
+        BackupStorage backend,
         Dto.BackupCreateRequest request
     )
     {
         var response = await _httpClient.PostAsJsonAsync(
-            WeaviateEndpoints.Backups(backend),
+            WeaviateEndpoints.Backups(backend.ToEnumMemberString()!),
             request,
             options: RestJsonSerializerOptions
         );
@@ -21,9 +21,11 @@ public partial class WeaviateRestClient
             ) ?? throw new WeaviateRestClientException();
     }
 
-    internal async Task<Dto.BackupListResponse> BackupList(string backend)
+    internal async Task<Dto.BackupListResponse> BackupList(BackupStorage backend)
     {
-        var response = await _httpClient.GetAsync(WeaviateEndpoints.Backups(backend));
+        var response = await _httpClient.GetAsync(
+            WeaviateEndpoints.Backups(backend.ToEnumMemberString()!)
+        );
         await response.EnsureExpectedStatusCodeAsync([200], "list backups");
         return await response.Content.ReadFromJsonAsync<Dto.BackupListResponse>(
                 WeaviateRestClient.RestJsonSerializerOptions
@@ -31,14 +33,14 @@ public partial class WeaviateRestClient
     }
 
     internal async Task<Dto.BackupCreateStatusResponse> BackupStatus(
-        string backend,
+        BackupStorage backend,
         string id,
         string? bucket = null,
         string? path = null
     )
     {
         var response = await _httpClient.GetAsync(
-            WeaviateEndpoints.BackupStatus(backend, id, bucket, path)
+            WeaviateEndpoints.BackupStatus(backend.ToEnumMemberString()!, id, bucket, path)
         );
         await response.EnsureExpectedStatusCodeAsync([200], "backup status");
         return await response.Content.ReadFromJsonAsync<Dto.BackupCreateStatusResponse>(
@@ -47,26 +49,26 @@ public partial class WeaviateRestClient
     }
 
     internal async Task BackupCancel(
-        string backend,
+        BackupStorage backend,
         string id,
         string? bucket = null,
         string? path = null
     )
     {
         var response = await _httpClient.DeleteAsync(
-            WeaviateEndpoints.BackupStatus(backend, id, bucket, path)
+            WeaviateEndpoints.BackupStatus(backend.ToEnumMemberString()!, id, bucket, path)
         );
         await response.EnsureExpectedStatusCodeAsync([200, 204], "backup cancel");
     }
 
     internal async Task<Dto.BackupRestoreResponse> BackupRestore(
-        string backend,
+        BackupStorage backend,
         string id,
         Dto.BackupRestoreRequest request
     )
     {
         var response = await _httpClient.PostAsJsonAsync(
-            WeaviateEndpoints.BackupRestore(backend, id),
+            WeaviateEndpoints.BackupRestore(backend.ToEnumMemberString()!, id),
             request,
             options: RestJsonSerializerOptions
         );
@@ -77,14 +79,14 @@ public partial class WeaviateRestClient
     }
 
     internal async Task<Dto.BackupRestoreStatusResponse> BackupRestoreStatus(
-        string backend,
+        BackupStorage backend,
         string id,
         string? bucket = null,
         string? path = null
     )
     {
         var response = await _httpClient.GetAsync(
-            WeaviateEndpoints.BackupRestoreStatus(backend, id, bucket, path)
+            WeaviateEndpoints.BackupRestoreStatus(backend.ToEnumMemberString()!, id, bucket, path)
         );
         await response.EnsureExpectedStatusCodeAsync([200], "backup restore status");
         return await response.Content.ReadFromJsonAsync<Dto.BackupRestoreStatusResponse>(
