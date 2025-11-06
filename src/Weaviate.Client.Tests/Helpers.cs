c727348ba8cea935980d3c88bdfab3f531152703
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-11-06 13:10:24 +0100

    Add gRPC support for WeaviateClient and implement NoOpGrpcChannel for testing

--- src/Weaviate.Client.Tests/Helpers.cs
+++ src/Weaviate.Client.Tests/Helpers.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using Google.Protobuf;
 
 namespace Weaviate.Client.Tests;
 
@@ -23,6 +25,62 @@ public static class Helpers
             .Replace(" ", "")
             .Replace(".", "");
         return char.ToUpper(name[0]) + name.Substring(1);
+    }
+
+    /// <summary>
+    /// Serializes a protobuf message to gRPC wire format.
+    /// Wire format: [compression flag (1 byte)][message length (4 bytes, big-endian)][protobuf message]
+    /// </summary>
+    /// <param name="message">The protobuf message to serialize</param>
+    /// <returns>Byte array in gRPC wire format</returns>
+    public static byte[] SerializeGrpcMessage(IMessage message)
+    {
+        var messageBytes = message.ToByteArray();
+        var grpcMessage = new byte[5 + messageBytes.Length];
+
+        // Compression flag (0 = no compression)
+        grpcMessage[0] = 0;
+
+        // Message length in big-endian (4 bytes)
+        System.Buffers.Binary.BinaryPrimitives.WriteUInt32BigEndian(
+            grpcMessage.AsSpan(1, 4),
+            (uint)messageBytes.Length
+        );
+
+        // Copy protobuf message
+        messageBytes.CopyTo(grpcMessage, 5);
+
+        return grpcMessage;
+    }
+
+    /// <summary>
+    /// Creates an HTTP/2 response message containing a gRPC-encoded protobuf message.
+    /// Suitable for mocking gRPC service responses in tests.
+    /// </summary>
+    /// <param name="message">The protobuf message to include in the response</param>
+    /// <param name="statusCode">HTTP status code (default: OK)</param>
+    /// <param name="grpcStatus">gRPC status code (default: 0 = OK)</param>
+    /// <returns>HttpResponseMessage configured for gRPC</returns>
+    public static HttpResponseMessage CreateGrpcResponse(
+        IMessage message,
+        HttpStatusCode statusCode = HttpStatusCode.OK,
+        int grpcStatus = 0
+    )
+    {
+        var grpcMessage = SerializeGrpcMessage(message);
+
+        var response = new HttpResponseMessage(statusCode)
+        {
+            Version = new Version(2, 0), // HTTP/2 is required for gRPC
+            Content = new ByteArrayContent(grpcMessage),
+        };
+
+        response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(
+            "application/grpc"
+        );
+        response.TrailingHeaders.Add("grpc-status", grpcStatus.ToString());
+
+        return response;
     }
 }
 
