ad099624742573cfc9795a0a27327353a2760b97
Michelangelo Partipilo <michelangelo@weaviate.io>
2025-11-26 17:23:54 +0100

    feat: Refactor WeaviateClient to remove token service factory and streamline async initialization

--- src/Weaviate.Client/WeaviateClient.cs
+++ src/Weaviate.Client/WeaviateClient.cs
@@ -21,7 +21,6 @@ public partial class WeaviateClient : IDisposable
 
     // Async initialization support
     private readonly Lazy<Task>? _initializationTask;
-    private readonly ITokenServiceFactory? _tokenServiceFactory;
     private readonly ClientConfiguration? _configForAsyncInit;
 
     public async Task<Models.MetaInfo> GetMeta(CancellationToken cancellationToken = default)
